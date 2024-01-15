2c540dc00e6b6913ea33f330bcd532cb9b0bf29f
stechmo <moritzstecher@gmx.de>
2024-01-15 09:22:35 +0100

    creating methods

--- lists.py
+++ lists.py
@@ -2,8 +2,8 @@
 # find max and min of a list
 # remove duplicates
 
-mylist = [4,10,16,3,54,32,4,16]
-mylist = list(dict.fromkeys(mylist))
-print(mylist)
-print(min(mylist))
-print(max(mylist))
+def minmax(inputlist):
+    return min(inputlist),max(inputlist)
+
+def removeduplicates(inputlist):
+    return list(dict.fromkeys(inputlist))
