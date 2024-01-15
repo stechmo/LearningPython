0b0a3f7724ab76704aa0f8ec72f646be7246b136
stechmo <moritzstecher@gmx.de>
2024-01-15 09:19:11 +0100

    Add list-exercise

---
+++ lists.py
@@ -0,0 +1,9 @@
+# List
+# find max and min of a list
+# remove duplicates
+
+mylist = [4,10,16,3,54,32,4,16]
+mylist = list(dict.fromkeys(mylist))
+print(mylist)
+print(min(mylist))
+print(max(mylist))
