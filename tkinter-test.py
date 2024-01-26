894b8a2ea229a6c291f4a7679e375e3c7a4d9b9a
stechmo <moritzstecher@gmx.de>
2024-01-26 19:38:38 +0100

    Tkinter

---
+++ tkinter-test.py
@@ -0,0 +1,10 @@
+import tkinter
+
+root = tkinter.Tk()
+
+# Creating a Label Widget
+myLabel = tkinter.Label(root, text="Hello World!")
+# Showing it onto the screen
+myLabel.pack()
+
+root.mainloop()
